076fe53a3e07b6b9f762b98d9b0d127da74ae713
Taylor Harvey <Taylor Harvey>
2017-03-27 20:04:35 -0400

    first commit

---
+++ assets/javascript/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'ipiHZy60FEgUieChuxRth6n2M',
+  consumer_secret: 'tivw4OVsC9iXGQwNuTaLyM3s9JO1uX3XCB224XBDau5lpd3Ars',
+  access_token_key: '803660575767150592-PNfSZtkDkjqUfbakQPBYZPKmefr8rW3',
+  access_token_secret: 'VTBUPxzzT7ZrmVSvZQFnBIrRnkK0ylRRSbUJLT60vlSwM',
+}
